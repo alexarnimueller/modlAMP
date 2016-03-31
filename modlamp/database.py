63d181deb5e59abb2a5969840b65bd96de6e94b2
Alex <alex.mueller@pharma.ethz.ch>
2016-03-31 17:08:22 +0200

    added new function query_experiments to query peptides DB

--- modlamp/database.py
+++ modlamp/database.py
@@ -8,8 +8,9 @@ This module incorporates functions to connect to the modlab internal peptide dat
 the different database tables.
 """
 
-from getpass import getpass
+import numpy as np
 import mysql.connector
+from getpass import getpass
 from mysql.connector import Error
 
 __author__ = "modlab"
@@ -63,9 +64,9 @@ def _connect():
 			return conn
 
 
-def query_sequences(table='modlab_experiments'):
+def query_sequences(table='modlab_peptides'):
 	"""
-	This function extracts all sequences stored in a table column "sequence" and returns them as a list.
+	This function extracts all sequences from a given table in the modlab peptide database.
 
 	:param table: the mysql database table to be queried
 	:return: a list of sequences as strings
@@ -80,14 +81,56 @@ def query_sequences(table='modlab_experiments'):
 	try:
 		conn = _connect()
 		cursor = conn.cursor()
-		cursor.execute('SELECT sequence FROM ' + table)
-
+		query = ("SELECT sequence FROM " + table)
+		cursor.execute(query)
 		row = cursor.fetchone()
 		rows = []
 		while row is not None:
-			rows.append(row[0].encode('utf-8','ignore').strip()) # encode from unicode to utf-8 string
+			rows.append(row[0].encode('utf-8', 'ignore').strip())  # encode from unicode to utf-8 string
 			row = cursor.fetchone()
 
+		return rows
+
+	except Error as e:
+		print(e)
+
+
+def query_experiments(table='modlab_experiments', columns=['sequence']):
+	"""
+	This function extracts experimental results from the modlab peptide database. All data from the given table and
+	column names is extracted and returned.
+
+	:param table: the mysql database table to be queried
+	:param columns: a list of the column names {str} to be extracted from the table
+	:return: a list of sequences as strings
+	:Example:
+
+	>>> data = query_experiments(table='modlab_experiments', columns=['sequence', 'MCF7_activity', 'Saureus_activity'])
+	Password: *********
+	Connecting to MySQL database...
+	connection established!
+	>>> data[:5]
+	array([['ILGTILGILKGL', None, 1.0],
+		   ['ILGTILGFLKGL', None, 1.0],
+       	   ['ILGNILGFLKGL', None, 1.0],
+       	   ['ILGQILGILKGL', None, 1.0],
+       	   ['ILGHILGYLKGL', None, 1.0]], dtype=object)
+
+    .. note::
+    	If 'None' appears as a value, this means no data was measured for this peptide and not that activity is None
+    	(inactive).
+	"""
+	try:
+		conn = _connect()
+		cursor = conn.cursor()
+		query = ("SELECT " + ', '.join(columns) + " FROM " + table)
+		cursor.execute(query)
+		rows = np.asarray(cursor.fetchall(), dtype='object')  # return data in a numpy array
+
+		for i, c in enumerate(rows[0]):
+			if isinstance(c, unicode):  # if a column is type 'unicode', it gets converted to 'utf-8'
+				for n, e in enumerate(rows[:, i]):
+					rows[:, i][n] = e.encode('utf-8', 'ignore').strip()
 		return rows
 
 	except Error as e:
