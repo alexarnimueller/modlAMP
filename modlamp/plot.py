abbc180035d8b04343f59c3186a6ea7338f3762a
Alex <alex.mueller@pharma.ethz.ch>
2016-04-07 16:39:00 +0200

    added function to produce sequence profile plots

--- modlamp/plot.py
+++ modlamp/plot.py
@@ -18,6 +18,7 @@ Function							Characteristics
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from modlamp.descriptors import PeptideDescriptor, GlobalDescriptor
 
 __author__ = "modlab"
 __docformat__ = "restructuredtext en"
@@ -135,11 +136,11 @@ def plot_3_features(x_values, y_values, z_values, targets=None, x_label='', y_la
 	:param x_values: Array of values of the feature to be plotted on the x-axis.
 	:param y_values: Array of values of the feature to be plotted on the y-axis.
 	:param z_values: Array of values of the feature to be plotted on the z-axis.
-	:param targets: List of target class values [string/binary] for the given feature data.
-	:param x_label: X-axis label.
-	:param y_label: Y-axis label.
-	:param z_label: Z-axis label.
-	:param filename: filename where to safe the plot. *default = None*
+	:param targets: List of target class values {string/binary} for the given feature data.
+	:param x_label: {str} X-axis label.
+	:param y_label: {str} Y-axis label.
+	:param z_label: {str} Z-axis label.
+	:param filename: {str} filename where to safe the plot. *default = None* -> show the plot
 	:return: A 3D feature scatter plot.
 	:Example:
 
@@ -149,32 +150,92 @@ def plot_3_features(x_values, y_values, z_values, targets=None, x_label='', y_la
 		:scale: 50 %
 	"""
 
-	colors = ['dodgerblue','firebrick','gold','lightgreen','navy','black'] # available colors
+	colors = ['dodgerblue', 'firebrick', 'gold', 'lightgreen', 'navy', 'black']  # available colors
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
 
 	if targets:
-		for n in list(set(targets)): # finding indices of the different targets in "targets" and plotting
-			t = np.array([i for i,j in enumerate(targets) if j == n])
-			xt = x_values[t] # find all values in x for the given target
-			yt = y_values[t] # find all values in y for the given target
-			zt = z_values[t] # find all values in y for the given target
-			ax.scatter(xt, yt, zt, c=colors[n], alpha=1., s=25, label='class '+str(n)) # plot 3Dscatter for this target group
+		for n in list(set(targets)):  # finding indices of the different targets in "targets" and plotting
+			t = np.array([i for i, j in enumerate(targets) if j == n])
+			xt = x_values[t]  # find all values in x for the given target
+			yt = y_values[t]  # find all values in y for the given target
+			zt = z_values[t]  # find all values in y for the given target
+			ax.scatter(xt, yt, zt, c=colors[n], alpha=1., s=25, label='class '+str(n))  # plot 3Dscatter for this target
 			ax.legend(loc='best')
 
-	else:
-		ax.scatter(x_values, y_values, z_values, c=colors[0], alpha=1., s=25) # plot 3Dscatter for this target group
+	else:  # plot 3Dscatter for this target group
+		ax.scatter(x_values, y_values, z_values, c=colors[0], alpha=1., s=25)
 
 	ax.set_xlabel(x_label, fontweight='bold')
 	ax.set_ylabel(y_label, fontweight='bold')
 	ax.set_zlabel(z_label, fontweight='bold')
-	ax.set_title('3D Feature Plot',fontsize=16,fontweight='bold')
+	ax.set_title('3D Feature Plot', fontsize=16, fontweight='bold')
 
 	if filename:
-		plt.savefig(filename,dpi=150)
+		plt.savefig(filename, dpi=150)
 	else:
 		plt.show()
 
-# TODO: function to plot profile of a given scale & window
+
+def plot_profile(sequence, window=5, scalename='eisenberg', filename=None):
+	""" Function to generate sequence profile plots of a given amino acid scale or a moment thereof.
+
+	.. note::
+		:func`plot_profile` can only plot one-dimensional amino acid scales given in :class:`PeptideDescriptor`.
+
+	:param sequence: {str} Peptide sequence for which the profile should be plotted.
+	:param window: {int, uneven} Window size for which the average value is plotted for the center amino acid.
+	:param scalename: {str} Amino acid scale to be used to describe the sequence.
+	:param filename: {str} Filename  where to safe the plot. *default = None* -> show the plot
+	:return: a profile plot of the input sequence interactively or with the specified *filename*
+	:Example:
+
+	>>> plot_profile('GLFDIVKKVVGALGSL', scalename='eisenberg')
+
+	.. image:: ../docs/static/profileplot.png
+		:scale: 50 %
+	"""
+	# check if given scale is defined in PeptideDescriptor
+	try:
+		D = PeptideDescriptor(sequence, scalename)
+	except TypeError:
+		print("\nError\nNo sequence given!")
+	except KeyError:
+		print("\nSorry\nThis function cannot calculate a profile for the given scale '%s'." % scalename)
+		print("Use the one dimensional scales given in the documentation for modlamp.descriptors.PeptideDescriptors")
+	else:
+		seq_data = list()
+		seq_profile = list()
+		for a in sequence:
+			seq_data.append(D.scale[a])  # describe sequence by given scale
+		i = 0  # AA index
+		while (i + window) < len(sequence):
+			seq_profile.append(np.mean(seq_data[i:(i + window + 1)]))  # append average value for given window
+			i += 1
+
+		# plot
+		fig, ax = plt.subplots()
+		x_range = range(int(window)/2 + 1, len(sequence)-int(window)/2)
+		line = ax.plot(x_range, seq_profile)
+		plt.setp(line, color='red', linewidth=2.0)
+
+		# axis labes and title
+		ax.set_xlabel('amino acid position', fontweight='bold')
+		ax.set_ylabel(scalename + ' value', fontweight='bold')
+		ax.set_title('Sequence Profile For ' + sequence, fontsize=16, fontweight='bold')
+		ax.text(max(x_range)/2 + 1, 1.05 * max(seq_profile), 'window size: ' + str(window), fontsize=12)
+
+		# only left and bottom axes, no box
+		ax.spines['right'].set_visible(False)
+		ax.spines['top'].set_visible(False)
+		ax.xaxis.set_ticks_position('bottom')
+		ax.yaxis.set_ticks_position('left')
+
+		# show or save plot
+		if filename:
+			plt.savefig(filename, dpi=150)
+		else:
+			plt.show()
+
 # TODO: helical wheel plot function?
