939a6fa063664f85dbd020abccde7166c5a0a822
Alex Müller <alex.mueller@pharma.ethz.ch>
2016-06-23 15:41:12 +0200

    updated Boman scale

--- modlamp/descriptors.py
+++ modlamp/descriptors.py
@@ -319,7 +319,7 @@ class GlobalDescriptor(object):
 		:return: array of descriptor values in the attribute :py:attr:`descriptor`
 		"""
 		D = {'L':-4.92,'I':-4.92,'V':-4.04,'F':-2.98,'M':-2.35,'W':-2.33,'A':-1.81,'C':-1.28,'G':-0.94,'Y':0.14,'T':2.57,
-			 'S':3.40,'H':4.66,'Q':5.54,'K':5.55,'N':6.64,'E':6.81,'D':8.72,'R':14.92}
+			 'S':3.40,'H':4.66,'Q':5.54,'K':5.55,'N':6.64,'E':6.81,'D':8.72,'R':14.92, 'P':0.}
 		desc = []
 		for seq in self.sequences:
 			val = []
