352a21f38a02c5f3f4ba99adb0dae38447a66309
Gisela <gisela.gabernet@pharma.ethz.ch>
2016-06-30 10:17:35 +0200

    Added a sequence to test core and ordered alphabetically tests

--- modlamp/tests/test_core.py
+++ modlamp/tests/test_core.py
@@ -4,7 +4,7 @@ from modlamp.descriptors import PeptideDescriptor
 
 
 class TestCore(unittest.TestCase):
-	sequences = ['GLFDIVKKVVGALG', 'GLFDIVKKVVGALG', 'GLFDIVKKVVGALK', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+	sequences = ['GLFDIVKKVVGALG', 'GLFDIVKKVVGALG', 'GLFDIVKKVVGALK', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'AGGURST']
 	r = Random(10, 20, 1)
 	r.sequences = sequences
 	l = Random(7, 28, 100)
@@ -12,21 +12,21 @@ class TestCore(unittest.TestCase):
 	d = PeptideDescriptor(l.sequences, 'eisenberg')
 	d.calculate_moment()
 
+	def test_filter_aa(self):
+		self.r.filter_aa(['C'])
+		self.assertEqual(len(self.r.sequences), 4)
+
 	def test_filter_duplicates(self):
 		self.r.filter_duplicates()
-		self.assertEqual(len(self.r.sequences), 2)
+		self.assertEqual(len(self.r.sequences), 3)
 
 	def test_filter_unnatural(self):
 		self.r.filter_unnatural()
-		self.assertNotIn('ABCDEFGHIJKLMNOPQRSTUVWXYZ', self.r.sequences)
+		self.assertNotIn(['ABCDEFGHIJKLMNOPQRSTUVWXYZ','AGGURST'], self.r.sequences)
 
 	def test_mutate(self):
 		self.r.mutate_AA(1, 1)
 		self.assertNotEqual(self.sequences, self.r.sequences)
-
-	def test_filter_aa(self):
-		self.r.filter_aa(['C'])
-		self.assertEqual(len(self.r.sequences), 3)
 
 	def test_rand_selection(self):
 		self.d.random_selection(10)
