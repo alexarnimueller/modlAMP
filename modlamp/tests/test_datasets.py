1c799588eb276a70fa5151fa1030c8c307e71c07
Alex Müller <alex.mueller@pharma.ethz.ch>
2016-07-12 09:56:02 +0200

    spaces

--- modlamp/tests/test_datasets.py
+++ modlamp/tests/test_datasets.py
@@ -3,25 +3,25 @@ from modlamp.datasets import load_helicalAMPset, load_AMPvsTMset
 
 class TestHelicalSet(unittest.TestCase):
 
-	data = load_helicalAMPset()
+    data = load_helicalAMPset()
 
-	def test_sequences(self):
-		self.assertEqual('NPATLMMFFK', self.data.sequences[3])
+    def test_sequences(self):
+        self.assertEqual('NPATLMMFFK', self.data.sequences[3])
 
-	def test_targets(self):
-		self.assertEqual([0, 0, 1, 1], [int(self.data.target[i]) for i in [0, 362, 363, 725]])
+    def test_targets(self):
+        self.assertEqual([0, 0, 1, 1], [int(self.data.target[i]) for i in [0, 362, 363, 725]])
 
 
 class TestTMSet(unittest.TestCase):
 
-	data = load_AMPvsTMset()
+    data = load_AMPvsTMset()
 
-	def test_sequences(self):
-		self.assertEqual('HGSIGAGVDW', self.data.sequences[5])
+    def test_sequences(self):
+        self.assertEqual('HGSIGAGVDW', self.data.sequences[5])
 
-	def test_targets(self):
-		self.assertEqual([0, 0, 1, 1], [int(self.data.target[i]) for i in [0, 205, 206, 411]])
+    def test_targets(self):
+        self.assertEqual([0, 0, 1, 1], [int(self.data.target[i]) for i in [0, 205, 206, 411]])
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
