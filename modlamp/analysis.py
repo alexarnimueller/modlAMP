a511a8b27bc24b248b461497a15451233535e831
Alex Müller <alex.mueller@pharma.ethz.ch>
2016-11-17 12:37:01 +0100

    class Global with method calc_aa_freq in analysis.py

--- modlamp/analysis.py
+++ modlamp/analysis.py
@@ -6,4 +6,48 @@
 
 This module can be used for diverse analysis of given peptide libraries.
 """
+import numpy as np
+from modlamp.core import count_aa
 
+
+class Global(object):
+    """
+    Base class for amino acid sequence library analysis
+    """
+    def __init__(self, library):
+        if type(library) == np.ndarray:
+            self.library = library
+        else:
+            self.library = np.array(library)
+
+        # check if library consists of sub-libraries
+        if len(self.library.shape) > 1:
+            self.libshape = self.library.shape[0]
+        else:
+            self.libshape = 1
+
+        self.aafreq = np.zeros((self.libshape, 20), dtype='float64')
+            
+    def calc_aa_freq(self):
+        """Method to get the frequency of every amino acid in the library. If the library consists of sub-libraries,
+        the frequencies of these are calculated independently.
+        
+        :return: {numpy.ndarray} amino acid frequencies in the attribute :py:attr:`aafreq`. The values are oredered
+            alphabetically.
+        :Example:
+        
+        >>> g = Global(sequences)
+        >>> g.calc_aa_freq()
+        >>> g.aafreq
+            array([[ 0.08250071,  0.        ,  0.02083928,  0.0159863 ,  0.1464459 ,
+                     0.04795889,  0.06622895,  0.0262632 ,  0.12988867,  0.        ,
+                     0.09192121,  0.03111619,  0.01712818,  0.04852983,  0.05937768,
+                     0.07079646,  0.04396232,  0.0225521 ,  0.05994862,  0.01855552]])
+        """
+        for l in range(self.libshape):
+            if self.libshape == 1:
+                concatseq = ''.join(self.library)
+            else:
+                concatseq = ''.join(self.library[l])
+            d_aa = count_aa(concatseq)
+            self.aafreq[l] = [v / float(len(concatseq)) for v in d_aa.values()]
--- modlamp/tests/test_analysis.py
+++ modlamp/tests/test_analysis.py
@@ -1,14 +1,19 @@
 import unittest
-from ..analysis import *
+from ..analysis import Global
 from ..core import read_fasta
 from os.path import abspath, join
 
 
 class TestAnalysis(unittest.TestCase):
-    sequences, names = read_fasta(join(abspath('.'), 'modlamp/tests/files/lib.fasta'))
+    sequences, _ = read_fasta(join(abspath('.'), 'modlamp/tests/files/lib.fasta'))
+    a = Global(sequences)
     
-    def test_there(self):
-        self.assertEqual(self.sequences, self.sequences)
+    def test_libshape(self):
+        self.assertEqual(self.a.library.shape[0], 246)
+        
+    def test_aa_freq(self):
+        self.a.calc_aa_freq()
+        self.assertAlmostEqual(self.a.aafreq[0, 0], 0.08250071)
         
 
 if __name__ == '__main__':
