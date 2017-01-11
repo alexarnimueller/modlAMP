f1dde5c778d338b29208c87903527fffd3229643
Alex Müller <alex.mueller@pharma.ethz.ch>
2017-01-11 14:34:40 +0100

    v2.6.4 with docs, setup and changelog

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('LICENSE') as f:
     lic = f.read()
 
 setup(name='modlamp',
-      version='2.6.3',
+      version='2.6.4',
       description='python package for in silico peptide design and QSAR studies',
       long_description=readme,
       author='Alex Müller, Gisela Gabernet',
