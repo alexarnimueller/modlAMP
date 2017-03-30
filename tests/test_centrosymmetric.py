f776fc31b303e3697fc158b7163fe4173e5121bd
alexarnimueller <alex.mueller@pharma.ethz.ch>
2017-03-30 19:11:14 +0200

    v3.0.0

--- tests/test_centrosymmetric.py
+++ tests/test_centrosymmetric.py
@@ -4,18 +4,26 @@ from modlamp.sequences import Centrosymmetric
 
 class TestCentrosymmetric(unittest.TestCase):
     S = Centrosymmetric(1)
-    S.generate_symmetric()
-
+    S.generate_sequences(symmetry='symmetric')
+    
     def test_block_symmetry(self):
-        self.assertEqual(self.S.sequences[0][0],self.S.sequences[0][6])
-        self.assertEqual(self.S.sequences[0][1],self.S.sequences[0][5])
-        self.assertEqual(self.S.sequences[0][2],self.S.sequences[0][4])
-
+        self.assertEqual(self.S.sequences[0][0], self.S.sequences[0][6])
+        self.assertEqual(self.S.sequences[0][1], self.S.sequences[0][5])
+        self.assertEqual(self.S.sequences[0][2], self.S.sequences[0][4])
+    
     def test_whole_symmetry(self):
-        self.assertEqual(self.S.sequences[0][0:6],self.S.sequences[0][7:13])
-
+        self.assertEqual(self.S.sequences[0][0:6], self.S.sequences[0][7:13])
+    
     def test_length(self):
-        self.assertIn(len(self.S.sequences[0]),(14,21))
+        self.assertIn(len(self.S.sequences[0]), (14, 21))
+
+
+class TestCentroAsymmetric(unittest.TestCase):
+    AS = Centrosymmetric(1)
+    AS.generate_sequences(symmetry='asymmetric')
+    
+    def test_whole_symmetry(self):
+        self.assertNotEqual(self.AS.sequences[0][0:6], self.AS.sequences[0][7:13])
 
 if __name__ == '__main__':
     unittest.main()
