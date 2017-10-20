8f30b57a528638ea9222d030e585e6127db997f5
Gisela Gabernet Garriga <gisela.gabernet@gmail.com>
2017-10-20 15:20:03 +0200

    paralelization of arc calculation

--- modlamp/descriptors.py
+++ modlamp/descriptors.py
@@ -87,6 +87,106 @@ def _one_crosscorr(seq, window, scale):
     except ZeroDivisionError:
         print("ERROR!\nThe chosen correlation window % i is larger than the sequence %s !" % (window, seq))
 
+def _one_arc(seq, modality, scale):
+    """ Privat function used for calculating arc descriptors for one sequence and AA scale. This function is used by
+    :py:func:`calculate_arc` method method of :py:class:`PeptideDescriptor`.
+
+    :param seq: {str} amino acid sequence to calculate descriptor for
+    :param scale: {str} amino acid scale to be used to calculate descriptor
+    :return: {numpy.array} calculated descriptor data
+    """
+    desc_mat = []
+    for aa in seq:
+        desc_mat.append(scale[aa])
+    desc_mat = np.asarray(desc_mat)
+
+    # Check descriptor dimension
+    desc_dim = desc_mat.shape[1]
+
+    # list to store descriptor values for all windows
+    allwindows_arc = []
+
+    if len(seq) > 18:
+        window = 18
+        # calculates number of windows in sequence
+        num_windows = len(seq) - window
+    else:
+        window = len(seq)
+        num_windows = 1
+
+    # loop through all windows
+    for j in range(num_windows):
+        # slices descriptor matrix into current window
+        window_mat = desc_mat[j:j + window, :]
+
+        # defines order of amino acids in helical projection
+        order = [0, 11, 4, 15, 8, 1, 12, 5, 16, 9, 2, 13, 6, 17, 10, 3, 14, 7]
+
+        # orders window descriptor matrix into helical projection order
+        ordered = []
+        for pos in order:
+            try:
+                ordered.append(window_mat[pos, :])
+            except:
+                # for sequences of len < 18 adding dummy vector with 2s, length of descriptor dimensions
+                ordered.append([2] * desc_dim)
+        ordered = np.asarray(ordered)
+
+        window_arc = []
+
+        # loop through pharmacophoric features
+        for m in range(desc_dim):
+            all_arcs = []  # stores all arcs that can be found of a pharmacophoric feature
+            arc = 0
+
+            for n in range(18):  # for all positions in helix, regardless of sequence length
+                if ordered[n, m] == 0:  # if position does not contain pharmacophoric feature
+                    all_arcs.append(arc)  # append previous arc to all arcs list
+                    arc = 0  # arc is initialized
+                elif ordered[n, m] == 1:  # if position contains pharmacophoric feature(PF), elongate arc by 20°
+                    arc += 20
+                elif ordered[n, m] == 2:  # if position doesn't contain amino acid:
+                    if ordered[n - 1, m] == 1:  # if previous position contained PF add 10°
+                        arc += 10
+                    elif ordered[n - 1, m] == 0:  # if previous position didn't contain PF don't add anything
+                        arc += 0
+                    elif ordered[
+                                n - 2, m] == 1:  # if previous position is empty then check second previous for PF
+                        arc += 10
+                    if n == 17:  # if we are at the last position check for position n=0 instead of next position.
+                        if ordered[0, m] == 1:  # if it contains PF add 10° extra
+                            arc += 10
+                    else:  # if next position contains PF add 10° extra
+                        if ordered[n + 1, m] == 1:
+                            arc += 10
+                        elif ordered[n + 1, m] == 0:
+                            arc += 0
+                        else:  # if next position is empty check for 2nd next position
+                            if n == 16:
+                                if ordered[0, m] == 1:
+                                    arc += 10
+                            else:
+                                if ordered[n + 2, m] == 1:
+                                    arc += 10
+
+            all_arcs.append(arc)
+            if not arc == 360:
+                arc0 = all_arcs.pop() + all_arcs[0]  # join first and last arc together
+                all_arcs = [arc0] + all_arcs[1:]
+
+            window_arc.append(np.max(all_arcs))  # append to window arcs the maximum arc of this PF
+        allwindows_arc.append(window_arc)  # append all PF arcs of this window
+
+    allwindows_arc = np.asarray(allwindows_arc)
+
+    if modality == 'max':
+        final_arc = np.max(allwindows_arc, axis=0)  # calculate maximum / mean arc along all windows
+    elif modality == 'mean':
+        final_arc = np.mean(allwindows_arc, axis=0)
+    else:
+        print 'modality is unknown, please choose between "max" and "mean"\n.'
+        sys.exit()
+    return final_arc
 
 def _charge(seq, ph=7.0, amide=False):
     """Calculates charge of a single sequence. The method used is first described by Bjellqvist. In the case of
@@ -880,104 +980,102 @@ class PeptideDescriptor(BaseDescriptor):
         >>> arc.descriptor
         array([[200, 160, 160,   0,   0]])
         """
-
-        desc = []
+        desc = Parallel(n_jobs=-1)(delayed(_one_arc)(seq, modality, self.scale) for seq in self.sequences)
 
         # Converts each of the amino acids to descriptor vector
         for seq in self.sequences:
-            desc_mat = []
-            for aa in seq:
-                desc_mat.append(self.scale[aa])
-            desc_mat = np.asarray(desc_mat)
 
-            # Check descriptor dimension
-            desc_dim = desc_mat.shape[1]
-
-            # list to store descriptor values for all windows
-            allwindows_arc = []
-
-            if len(seq) > 18:
-                window = 18
-                # calculates number of windows in sequence
-                num_windows = len(seq) - window
-            else:
-                window = len(seq)
-                num_windows = 1
-
-            # loop through all windows
-            for j in range(num_windows):
-                # slices descriptor matrix into current window
-                window_mat = desc_mat[j:j + window, :]
-
-                # defines order of amino acids in helical projection
-                order = [0, 11, 4, 15, 8, 1, 12, 5, 16, 9, 2, 13, 6, 17, 10, 3, 14, 7]
-
-                # orders window descriptor matrix into helical projection order
-                ordered = []
-                for pos in order:
-                    try:
-                        ordered.append(window_mat[pos, :])
-                    except:
-                        # for sequences of len < 18 adding dummy vector with 2s, length of descriptor dimensions
-                        ordered.append([2] * desc_dim)
-                ordered = np.asarray(ordered)
-
-                window_arc = []
-
-                # loop through pharmacophoric features
-                for m in range(desc_dim):
-                    all_arcs = []  # stores all arcs that can be found of a pharmacophoric feature
-                    arc = 0
-
-                    for n in range(18):  # for all positions in helix, regardless of sequence length
-                        if ordered[n, m] == 0:  # if position does not contain pharmacophoric feature
-                            all_arcs.append(arc)  # append previous arc to all arcs list
-                            arc = 0  # arc is initialized
-                        elif ordered[n, m] == 1:  # if position contains pharmacophoric feature(PF), elongate arc by 20°
-                            arc += 20
-                        elif ordered[n, m] == 2:  # if position doesn't contain amino acid:
-                            if ordered[n - 1, m] == 1:  # if previous position contained PF add 10°
-                                arc += 10
-                            elif ordered[n - 1, m] == 0:  # if previous position didn't contain PF don't add anything
-                                arc += 0
-                            elif ordered[
-                                        n - 2, m] == 1:  # if previous position is empty then check second previous for PF
-                                arc += 10
-                            if n == 17:  # if we are at the last position check for position n=0 instead of next position.
-                                if ordered[0, m] == 1:  # if it contains PF add 10° extra
-                                    arc += 10
-                            else:  # if next position contains PF add 10° extra
-                                if ordered[n + 1, m] == 1:
-                                    arc += 10
-                                elif ordered[n + 1, m] == 0:
-                                    arc += 0
-                                else:  # if next position is empty check for 2nd next position
-                                    if n == 16:
-                                        if ordered[0, m] == 1:
-                                            arc += 10
-                                    else:
-                                        if ordered[n + 2, m] == 1:
-                                            arc += 10
-
-                    all_arcs.append(arc)
-                    if not arc == 360:
-                        arc0 = all_arcs.pop() + all_arcs[0]  # join first and last arc together
-                        all_arcs = [arc0] + all_arcs[1:]
-
-                    window_arc.append(np.max(all_arcs))  # append to window arcs the maximum arc of this PF
-                allwindows_arc.append(window_arc)  # append all PF arcs of this window
-
-            allwindows_arc = np.asarray(allwindows_arc)
-
-            if modality == 'max':
-                final_arc = np.max(allwindows_arc, axis=0)  # calculate maximum / mean arc along all windows
-            elif modality == 'mean':
-                final_arc = np.mean(allwindows_arc, axis=0)
-            else:
-                print 'modality is unknown, please choose between "max" and "mean"\n.'
-                sys.exit()
-
-            desc.append(final_arc)
+            # desc_mat = []
+            # for aa in seq:
+            #     desc_mat.append(self.scale[aa])
+            # desc_mat = np.asarray(desc_mat)
+			#
+            # # Check descriptor dimension
+            # desc_dim = desc_mat.shape[1]
+			#
+            # # list to store descriptor values for all windows
+            # allwindows_arc = []
+			#
+            # if len(seq) > 18:
+            #     window = 18
+            #     # calculates number of windows in sequence
+            #     num_windows = len(seq) - window
+            # else:
+            #     window = len(seq)
+            #     num_windows = 1
+			#
+            # # loop through all windows
+            # for j in range(num_windows):
+            #     # slices descriptor matrix into current window
+            #     window_mat = desc_mat[j:j + window, :]
+			#
+            #     # defines order of amino acids in helical projection
+            #     order = [0, 11, 4, 15, 8, 1, 12, 5, 16, 9, 2, 13, 6, 17, 10, 3, 14, 7]
+			#
+            #     # orders window descriptor matrix into helical projection order
+            #     ordered = []
+            #     for pos in order:
+            #         try:
+            #             ordered.append(window_mat[pos, :])
+            #         except:
+            #             # for sequences of len < 18 adding dummy vector with 2s, length of descriptor dimensions
+            #             ordered.append([2] * desc_dim)
+            #     ordered = np.asarray(ordered)
+			#
+            #     window_arc = []
+			#
+            #     # loop through pharmacophoric features
+            #     for m in range(desc_dim):
+            #         all_arcs = []  # stores all arcs that can be found of a pharmacophoric feature
+            #         arc = 0
+			#
+            #         for n in range(18):  # for all positions in helix, regardless of sequence length
+            #             if ordered[n, m] == 0:  # if position does not contain pharmacophoric feature
+            #                 all_arcs.append(arc)  # append previous arc to all arcs list
+            #                 arc = 0  # arc is initialized
+            #             elif ordered[n, m] == 1:  # if position contains pharmacophoric feature(PF), elongate arc by 20°
+            #                 arc += 20
+            #             elif ordered[n, m] == 2:  # if position doesn't contain amino acid:
+            #                 if ordered[n - 1, m] == 1:  # if previous position contained PF add 10°
+            #                     arc += 10
+            #                 elif ordered[n - 1, m] == 0:  # if previous position didn't contain PF don't add anything
+            #                     arc += 0
+            #                 elif ordered[
+            #                             n - 2, m] == 1:  # if previous position is empty then check second previous for PF
+            #                     arc += 10
+            #                 if n == 17:  # if we are at the last position check for position n=0 instead of next position.
+            #                     if ordered[0, m] == 1:  # if it contains PF add 10° extra
+            #                         arc += 10
+            #                 else:  # if next position contains PF add 10° extra
+            #                     if ordered[n + 1, m] == 1:
+            #                         arc += 10
+            #                     elif ordered[n + 1, m] == 0:
+            #                         arc += 0
+            #                     else:  # if next position is empty check for 2nd next position
+            #                         if n == 16:
+            #                             if ordered[0, m] == 1:
+            #                                 arc += 10
+            #                         else:
+            #                             if ordered[n + 2, m] == 1:
+            #                                 arc += 10
+			#
+            #         all_arcs.append(arc)
+            #         if not arc == 360:
+            #             arc0 = all_arcs.pop() + all_arcs[0]  # join first and last arc together
+            #             all_arcs = [arc0] + all_arcs[1:]
+			#
+            #         window_arc.append(np.max(all_arcs))  # append to window arcs the maximum arc of this PF
+            #     allwindows_arc.append(window_arc)  # append all PF arcs of this window
+			#
+            # allwindows_arc = np.asarray(allwindows_arc)
+			#
+            # if modality == 'max':
+            #     final_arc = np.max(allwindows_arc, axis=0)  # calculate maximum / mean arc along all windows
+            # elif modality == 'mean':
+            #     final_arc = np.mean(allwindows_arc, axis=0)
+            # else:
+            #     print 'modality is unknown, please choose between "max" and "mean"\n.'
+            #     sys.exit()
 
             if append:
                 self.descriptor = np.hstack((self.descriptor, np.array(desc)))
