fba34724c933eb3802d742cc6f79d15f458337b7
Alex Müller <alex.mueller@pharma.ethz.ch>
2019-03-04 20:46:32 +0100

    v4.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(name='modlamp',
-      version='4.0.0',
+      version='4.0.1',
       description='python package for in silico peptide design and QSAR studies',
       long_description=readme,
       author='Alex Müller, Gisela Gabernet',
